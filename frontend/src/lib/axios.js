a7f7a8403101add4d219ca677217df34b6fedd3d
abhicoder237 <abhirock3127@gmail.com>
2025-02-08 20:52:56 +0530

    Delete frontend directory

--- frontend/src/lib/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api", // replace with your backend server URL
-    withCredentials : true, 
-})
